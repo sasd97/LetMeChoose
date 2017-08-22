23cde346dd7d020794cc89c01d2410c9fea808eb
alexander <st235@yandex.ru>
2017-08-22 23:37:57 +0300

    Implemented rx libraries

--- app/src/main/java/sasd97/java_blog/xyz/letmechoose/domain/models/IdeaModel.java
+++ app/src/main/java/sasd97/java_blog/xyz/letmechoose/domain/models/IdeaModel.java
@@ -7,4 +7,20 @@ package sasd97.java_blog.xyz.letmechoose.domain.models;
 public class IdeaModel {
 
     private String description;
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("IdeaModel{");
+        sb.append("description='").append(description).append('\'');
+        sb.append('}');
+        return sb.toString();
+    }
 }
